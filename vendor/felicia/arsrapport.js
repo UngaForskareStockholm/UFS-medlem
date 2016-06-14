450d2572d425c8857bc0b1f210003431f39ad170
kotteliten <kotteliten@gmail.com>
2016-06-14 14:41:06 +0200

    new file

--- vendor/felicia/arsrapport.js
+++ vendor/felicia/arsrapport.js
@@ -1,17 +1,10 @@
-console.log ("laddar arsrapport");
 var 
 	$konto = $('#konto'),
 	$bankclear = $('#bankclear');
-$konto.change(             
-console.log ("test"),
-     function () {
-        console.log ("test2"); if ($('#konto').val() == 'bankkonto') {
-            
-            $('#bankclear').removeAttr('disabled');
-        } 
-        else {
-                        console.log ("test3");
-            $('#bankclear').attr('disabled', 'disabled').val('');
-        }
-    } 
-).trigger('change'); // added trigger to calculate initial state
+$konto.change(function () {
+	if ($konto.val() == 'bankkonto') {
+		$bankclear.removeAttr('disabled');
+	} else {
+		$bankclear.attr('disabled', 'disabled').val('');
+	}
+}).trigger('change'); // added trigger to calculate initial state
